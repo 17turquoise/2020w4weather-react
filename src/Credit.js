bf26dc66d958981c24acdf6d05bf61c88f13ff91
17turquoise <71997148+17turquoise@users.noreply.github.com>
2020-12-28 14:08:27 +0900

    Revised credit.js to put white space

--- src/Credit.js
+++ src/Credit.js
@@ -5,21 +5,21 @@ export default function Credit() {
   return (
     <div className="Credit">
       <small className="credit">
-        This project was coded by
+        This project was coded by&nbsp;
         <a
           href="https://www.linkedin.com/in/ina-pae-72800978/"
           target="”_blank”"
         >
-          Ina Pae
+          Ina Pae&nbsp;
         </a>
-        and is
+        and is&nbsp;
         <a
           href="https://github.com/17turquoise/2020w4weather-react"
           target="”_blank”"
         >
-          open-sourced on GitHub
+          open-sourced on GitHub&nbsp;
         </a>
-        and hosted on
+        and hosted on&nbsp;
         <a href="https://adoring-raman-afda76.netlify.app/" target="”_blank”">
           Netlify.
         </a>
