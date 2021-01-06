5d4d6aeb4e267011cf13588e0a7ac90d67ea3d0a
17turquoise <71997148+17turquoise@users.noreply.github.com>
2021-01-06 22:29:24 +0900

    Modified Search button

--- src/Search.js
+++ src/Search.js
@@ -5,10 +5,22 @@ import { Form, Button } from "react-bootstrap";
 export default function Search() {
   return (
     <Form>
-      <label>
-        <input type="text" name="name" placeholder="Enter a City...:)" />
-      </label>
-      <Button type="submit">Search</Button>
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Enter a City...:)"
+            className="form-control"
+          />
+        </div>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary"
+          ></input>
+        </div>{" "}
+      </div>
     </Form>
   );
 }
